ca4170555ff25063d1e745e39adab418febc43ff
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-12-31 18:06:48 +0530

    welcome component created

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Info {
+  icon: string;
+  heading: string;
+  desc: string;
+}
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
+  homeText: Info[] = [
+    { icon: 'downhill_skiing', heading: 'Activity', desc: 'Stay active and enjoy better health and more fun!' },
+    { icon: 'groups', heading: 'Community', desc: 'Get to know other people who share your passion!' },
+    { icon: 'sports_kabaddi', heading: 'Chanllenges', desc: 'Never stop! Dive into new challenges everyday' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
