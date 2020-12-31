499cf32203d3586a9cf4c24f1ea719e674beb92e
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-12-31 17:26:48 +0530

    navigation made responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Links {
+  label: string;
+  route: string;
+  emojiName: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'fitness-tracker';
+  navigationLinks: Links[] = [
+    { label: 'Signup', route: '/signup', emojiName: 'face' },
+    { label: 'Login', route: '/login', emojiName: 'input' },
+    { label: 'Training', route: '/training', emojiName: 'fitness_center' },
+  ];
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   imports: [
@@ -14,6 +18,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatDatepickerModule,
     MatNativeDateModule,
     MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
   ],
   exports: [
     MatButtonModule,
@@ -22,6 +30,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatDatepickerModule,
     MatNativeDateModule,
     MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
   ],
 })
 export class MaterialModule {}
