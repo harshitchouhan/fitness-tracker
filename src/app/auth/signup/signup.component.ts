314afe6fdb2738da751b625eed93eca4b14ef330
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-12-31 15:38:58 +0530

    sign form completed

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -8,17 +8,20 @@ interface Form {
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  maxDate: Date = new Date();
+  minDate: Date = new Date();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    this.minDate.setFullYear(this.minDate.getFullYear() - 50);
   }
 
   onSubmit(form: Form) {
-    console.log(form)
+    console.log(form);
   }
-
 }
